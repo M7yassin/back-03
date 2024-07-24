6d520f27535dcd3a7755d54f76a44d869e2c83ce
M7yassin <mohammd.yassin72@gmail.com>
2024-07-24 12:19:40 +0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,46 @@
+const express= require("express")
+const app = express()
+const port = process.env.PORT||3000
+
+app.set('view engine','hbs');
+const path = require ("path")
+const publicDirectory =  path.join(__dirname , '../public')
+app.use (express.static (publicDirectory))
+
+const viewsDirectory = path.join (__dirname , '../templates/views')
+app.set('views', viewsDirectory);
+
+
+var hbs = require('hbs');
+const partialsPath = path.join(__dirname , "../Templates/partials")
+hbs.registerPartials(partialsPath)
+
+app.get('/',(req,res)=>{
+    res.render('index',{
+        title: "HOME",
+        desc:"This is Home Page"
+    }
+    )
+})
+app.get('/service',(req,res)=>{
+    res.render('service',{
+        title: "SERVICE",
+        name:"Mohammed",
+        city:"Tartous",
+        age:"24",
+        img1: "images/trainer.jpg"
+    }
+    )
+})
+app.get ('/team' , (req,res) => {
+    res.render('team' , {
+        title : "TEAM",
+        name: "Shaza",
+        city:"Damascus",
+        age: 25,
+        img2: "images/trainer2.jpg"
+    })
+})
+app.listen(port,()=>{
+    console.log("app is listening on port 3000")
+})
